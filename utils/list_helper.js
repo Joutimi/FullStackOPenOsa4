80d16caa0e128d1cc57ba997c1c34d7384b0c5b9
Joutimi <timijouppila@hotmail.com>
2023-04-11 12:01:54 +0300

    4.6 ja 4.7 valmiit. Toimivat tehtävänannon mukaisesti

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+  const _ = require('lodash')
+  
   const dummy = (blogs) => {
     return 1
   }
@@ -10,7 +12,42 @@
     console.log('likesAmount: ', likesAmount)
     return likesAmount
   }
+
+  const favoriteBlog = (blogs) => {
+    console.log('blogs lenght: ', blogs.length)
+    if(blogs.length != 0) {
+      const mostLikes = blogs.reduce(
+        (prev, current) => {
+          return prev.likes > current.likes ? prev : current
+      })
+    return mostLikes
+    } else return 0
+  }
+
+  const mostBlogs = (blogs) => {
+    var blogsSummed = _(blogs)
+      .groupBy('author')
+      .map(function(items, author) {
+        return { 
+          author: author,
+          blogs: items.length
+        }
+      }).value()
+    return favoriteBlog(blogsSummed)
+  }
+
+  const mostLikes = (blogs) => {
+    var likesSummed = _(blogs)
+      .groupBy('author')
+      .map(function(items, author) {
+        return {
+          author: author,
+          likes: _.sumBy(items, function(item) {return item.likes})
+        }
+      })
+    return favoriteBlog(likesSummed)
+  }
   
   module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
   }
