a028e81a1954d905de5fd6773fa040f7e9f9f07f
Joutimi <timijouppila@hotmail.com>
2023-04-14 14:50:49 +0300

    4.8-4.12 valmiit ja toimivat tehtävänannon mukaisesti

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,22 +2,33 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 // Get kaikki postaukset, palauttaa JSON muotoisen
-blogsRouter.get('/', (request, response) => {
-	Blog
-		.find({})
-		.then(blogs => {
-			response.json(blogs)
-		})
+blogsRouter.get('/', async (request, response) => {
+	const blogs = await Blog.find({})
+	response.json(blogs)
+
+
 })
 
 // Uuden postauksen postaus
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', (request, response, next) => {
 	const blog = new Blog(request.body)
+	if (!blog.likes) {
+		blog.likes = 0
+	}
+
+	if (!blog.title || !blog.url) {
+		return response.status(400).json({
+			error: 'bad request'
+		})
+	}
+
 	blog
 		.save()
 		.then(result => {
 			response.status(201).json(result)
 		})
+	
+	.catch(error => next.error)
 })
 
 
